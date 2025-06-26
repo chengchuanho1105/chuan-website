1ae061b22b13b4919ed1a4e29ddeee3e8c830b17
chengchuanho1105 <chengchuanho1105@gmail.com>
2025-06-26 14:07:18 +0800

    SEO-icon1-2

--- src/brands/seo/crazyclown.ts
+++ src/brands/seo/crazyclown.ts
@@ -3,8 +3,8 @@
 export const seo = {
   title: 'CrazyClown - 不瘋，不成團！',
   link: [
-    { rel: 'icon', href: 'assets/media/crazyclown/logo.png', type: 'image/png' },
-    { rel: 'apple-touch-icon', href: 'assets/media/crazyclown/logo.png' },
+    { rel: 'icon', href: 'https://chuan.life/assets/media/crazyclown/logo.png', type: 'image/png' },
+    { rel: 'apple-touch-icon', href: 'https://chuan.life/assets/media/crazyclown/logo.png' },
   ],
   meta: [
     {
@@ -18,7 +18,7 @@ export const seo = {
       property: 'og:description',
       content: '在這裡，每場遊戲都是一場瘋狂表演，這裡是 Crazy_Clown！期待你的加入！',
     },
-    { property: 'og:image', content: 'assets/media/crazyclown/ogimg.png' },
+    { property: 'og:image', content: 'https://chuan.life/assets/media/crazyclown/ogimg.png' },
     { property: 'og:url', content: 'https://chuan.life/crazyclown' },
     { property: 'og:type', content: 'website' },
     { name: 'twitter:card', content: 'summary_large_image' },
@@ -27,6 +27,6 @@ export const seo = {
       name: 'twitter:description',
       content: '在這裡，每場遊戲都是一場瘋狂表演，這裡是 Crazy_Clown！期待你的加入！',
     },
-    { name: 'twitter:image', content: 'assets/media/crazyclown/ogimg.png' },
+    { name: 'twitter:image', content: 'https://chuan.life/assets/media/crazyclown/ogimg.png' },
   ],
 }
